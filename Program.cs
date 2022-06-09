7cc4f7144766ef86404b74cecac4bb60d2b14d7e
Stepan_Suprunov <ipsuprunovso@mail.ru>
2022-06-09 20:04:40 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Одно число должно быть квадратом второго
+Console.WriteLine("Введите число 1");
+int Number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int Number2 = int.Parse(Console.ReadLine());
+if (Number1 == Number2 * Number2)
+{
+    Console.WriteLine("Вероно");
+}
+else
+{
+    Console.WriteLine("Не верно");
+}
